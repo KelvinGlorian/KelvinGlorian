e94a6dec88f7c20c33f21adcc99ffd133c18ff5c
KelvinGlorian <pamatuhun@gmai.com>
2024-09-19 10:43:42 +0700

    Initial commit

---
+++ application/views/Beranda/index.php
@@ -0,0 +1,97 @@
+
+<!-- header -->
+<div class="container"> 
+<div id="carouselExampleIndicators" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/images/dw1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/dw2.png" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/dw3.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+<!-- end header -->
+
+</div>
+ <! -- end header -->
+<section class="about" id="about">
+ <! -- about -->
+ <div class="container">
+    <div class="row-5">
+            <div class="col-md-12 text-center">
+              <h2>ABOUT</h2>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+    <div class="col-md-5">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis dolores veniam minus!</p>
+    </div>
+    <div class="col-md-5">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis dolores veniam minus!</p>
+    </div>
+</div>
+</div>
+</section>
+ <! -- end about -->
+ 
+<! -- kontak -->
+<section class="kontak" id="kontak"> 
+ <div class="container">
+    <div class="row mt-5">
+        <div class="col-md-12 text-center">
+        
+              <h2>KONTAK</h2>
+              <hr>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+         <div class="col-md-5">
+        <form>
+  <div class="form-group">
+    <label for="nama">Nama</label>
+    <input type="text" class="form-control" id="nama" placeholder="Masukkan Nama">
+    
+  </div>
+  <div class="form-group">
+    <label for="nama">Email</label>
+    <input type="text" class="form-control" id="email" placeholder="Masukkan Email">
+    
+  </div>
+  <div class="form-group">
+    <label for="nama">Alamat</label>
+    <input type="text" class="form-control" id="alamat" placeholder="Masukkan Alamat">
+    
+  </div>
+  <div class="form-group mb-3">
+    <label for="nama">Pesan</label>
+    <textarea type="textarea" class="form-control" id="pesan" placeholder="Masukkan Pesan"></textarea>
+    
+  </div>
+  
+  
+  <button type="submit" class="btn btn-primary">Kirim pesan</button>
+</form>
+    </div>
+ </div>
+</div>
+</div>
+</section>
+ <! -- end kontak -->
